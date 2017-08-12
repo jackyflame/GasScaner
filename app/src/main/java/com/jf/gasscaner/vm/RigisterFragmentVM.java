fd51ea39d83cde98c253ca7febeeb0b987871abb
Haozi <263667227@qq.com>
2017-08-12 11:42:25 +0800

    更新UI身份证数据联动；

--- app/src/main/java/com/jf/gasscaner/vm/RigisterFragmentVM.java
+++ app/src/main/java/com/jf/gasscaner/vm/RigisterFragmentVM.java
@@ -3,13 +3,17 @@ package com.jf.gasscaner.vm;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.databinding.Bindable;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.jf.gasscaner.BR;
 import com.jf.gasscaner.R;
 import com.jf.gasscaner.base.vm.BaseVM;
+import com.speedata.libid2.IDInfor;
 
 /**
  * Created by admin on 2017/8/9.
@@ -20,11 +24,30 @@ public class RigisterFragmentVM extends BaseVM{
     private Activity activity;
     /**当前序列号字母列表*/
     private String[] provinceArray;
-
     private String carProvinceMark;
+
+    private IDInfor idInfor;
 
     public RigisterFragmentVM(FragmentActivity activity) {
         this.activity = activity;
+
+        idInfor = new IDInfor();
+        idInfor.setName("张三");
+        idInfor.setNum("510622198805052211");
+        idInfor.setSex("男");
+        idInfor.setNation("汉族");
+        idInfor.setAddress("四川省成都市成华区将军路223号");
+        idInfor.setYear("1988");
+        idInfor.setMonth("05");
+        idInfor.setDay("05");
+        Bitmap bmp= BitmapFactory.decodeResource(activity.getResources(), R.mipmap.ic_launcher);
+        idInfor.setBmps(bmp);
+        //tvIDInfor.setText("姓名:" + idInfor1.getName() + "\n身份证号：" + idInfor1.getNum()
+        //        + "\n性别：" + idInfor1.getSex()
+        //        + "\n民族：" + idInfor1.getNation() + "\n住址:"
+        //        + idInfor1.getAddress() + "\n出生：" + idInfor1.getYear() + "年" + idInfor1
+        //        .getMonth() + "月" + idInfor1.getDay() + "日" + "\n有效期限：" + idInfor1
+        //        .getDeadLine());
     }
 
     public void onCarBandClick(View view){
@@ -55,5 +78,15 @@ public class RigisterFragmentVM extends BaseVM{
     public void setCarProvinceMark(String carProvinceMark) {
         this.carProvinceMark = carProvinceMark;
         notifyPropertyChanged(BR.carProvinceMark);
+    }
+
+    @Bindable
+    public IDInfor getIdInfor() {
+        return idInfor;
+    }
+
+    public void setIdInfor(IDInfor idInfor) {
+        this.idInfor = idInfor;
+        notifyPropertyChanged(BR.idInfor);
     }
 }
